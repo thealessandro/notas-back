12f1f7894f545993169a7adb461112a451ba9aae
Alessandro Menezes <alessandromenezes22@gmail.com>
2017-07-17 00:14:14 -0300

    Update NotasApplicationTests

--- src/test/java/br/com/kyxadious/notas/back/NotasApplicationTests.java
+++ src/test/java/br/com/kyxadious/notas/back/NotasApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class NotasApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
-
 }
